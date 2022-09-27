9c0e7365a5e27cf5707a3020565493dff5c4ceae
Nariman Huseynov <hnariman@gmail.com>
2022-09-27 04:03:05 +0400

    structured

---
+++ src/actions/mod.rs
@@ -0,0 +1,35 @@
+
+pub fn create(vars:&str) { 
+    println!("create : {vars}"); 
+    todo!();
+}
+
+pub fn read(vars:&str)   { 
+    println!("read : {vars}"); 
+    todo!();
+}
+
+pub fn update(vars:&str) { 
+    println!("update : {vars}"); 
+    todo!();
+}
+
+pub fn delete(vars:&str) {
+    println!("delete : {vars}"); 
+    todo!();
+}
+
+pub fn all(vars:&str) {
+    println!("get all : {vars}"); 
+    todo!();
+}
+
+pub fn help() {
+    println!("this is some help "); 
+    todo!();
+}
+
+pub fn unknown() {
+    println!("unknown argument, plase call --help"); 
+    todo!();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,29 @@
+mod actions;
 use std::env;
+use std::result::{Result};
+use std::fmt::{Error};
 
-fn create() { println!("create"); }
-fn read()   { println!("read"); }
-fn update() { println!("update"); }
-fn delete() { println!("delete"); }
-fn unknown() { println!("unknown"); }
-
-
-fn main() {
+fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
+    let vars;
+    
+    //TODO: I need proper error checking here
+    // if args.len() >2 {
+    //     vars = args[2..].join(" ");
+    // } else {
+    //     vars = String::from("");
+    // }
+
+    let vars = &args[2..].join(" ").chain_err(|| "problems");
     // converting String into slice:
-    let command = match &args[1][..]{
-        "c" => create(),
-        "r" => read(),
-        "u" => update(),
-        "d" => delete(),
-        _ => unknown(),
-    };
-
-    // let result = match command { };
-
-    // println!("command is : {:#?}",command);
+    match &args[1][..]{
+        "-c" => actions::create(vars),
+        "-r" => actions::read(vars),
+        "-u" => actions::update(vars),
+        "-d" => actions::delete(vars),
+        "-a" => actions::all(vars),
+        "-h" => actions::help(),
+        _ => actions::unknown(),
+    }
+    Ok(())
 }
