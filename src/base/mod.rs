6e21cbd0ccdfda24b3360f61a6b6587fe824b060
Nariman Huseynov <hnariman@gmail.com>
2022-09-29 02:20:36 +0400

    added cli table view

--- src/base/mod.rs
+++ src/base/mod.rs
@@ -1,11 +1,15 @@
 // imports 
 use rusqlite::{Connection};
+use cli_table::{format::Justify, print_stdout, Table, WithTitle};
 
 // structs
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Table)]
 pub struct Item {
+    #[table(title="ID", justify="Justify::Center")]
     pub id: i32,
+    #[table(title="Note", justify="Justify::Center")]
     pub note: String,
+    #[table(title="Done", justify="Justify::Center")]
     pub done: bool
 }
 
@@ -63,68 +67,11 @@ impl Storage {
 
     pub fn print_items(&mut self){
         let _ = &mut self.get_all_items();
-        for item in &self.items {
-            let Item{id, note, done} = item;
-            println!("{}. {} - {}", id, note, done);
-        }
+        print_stdout(self.items.with_title()).expect("unable to print table");
+        // for item in &self.items {
+        //     let Item{id, note, done} = item;
+        //     println!("{}. {} - {}", id, note, done);
+        // }
     }
 }
-
-// pub fn init() -> &'static Connection{
-//     let db:Connection = Connection::open("items.db").unwrap();
-//     &db
-// }
-
-// // craete sqlite table
-// pub fn create_table(){
-//     let db = init();
-//     db.execute(
-//         "create table if not exists todo (
-//            id integer primary key,
-//            note text not null,
-//            done boolean
-//         )",(),
-//     ).unwrap();
-// }
-
-// // create todo item
-// pub fn create_item(note:&str) {
-//     let db = init();
-
-//     let mut item = Item::default();
-//     item.note = note.to_string();
-//     db.execute("INSERT INTO todo (note, done) VALUES (?1, ?2)", (&item.note, &item.done)).unwrap();
-// }
-
-// // get all todo items
-// pub fn get_all_items() -> Vec<Item>{
-//     let db = init();
-//     let mut query = db.prepare("SELECT id, note, done FROM todo").unwrap();
-
-//     let rows = query.query_map([], |row| {
-//         Ok(Item{
-//             id:row.get(0)?,
-//             note:row.get(1)?,
-//             done:row.get(2)?,
-//         })
-//     }).unwrap();
-
-//     let mut result : Vec<Item> = Vec::new();
-
-//     for row in rows {
-//         result.push(row.unwrap());
-//     }
-//     result
-// }
-
-// pub fn print_rows(rows: Vec<Item>){
-//     let db = init();
-//     let items = get_all_items();
-
-//     for item in items {
-//         let Item{id, note, done} = item;
-//         println!("{}. {} - {}", id, note, done);
-//     }
-
-// }
 
